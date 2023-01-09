fb7d5d2a75f1e2b1c7312a688172915700ca030e
mayank4singh <mailatmausam@gmail.com>
2023-01-09 20:59:35 +0530

    first commit

---
+++ app/src/main/java/com/mayank/buttontoast/MainActivity.java
@@ -0,0 +1,36 @@
+package com.mayank.buttontoast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    Button btn, btn2;
+@Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn= findViewById(R.id.red);
+        btn2=findViewById(R.id.black);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,"i am red short",Toast.LENGTH_SHORT).show();
+            }
+        });
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,"i am black long",Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+
+    }
+}
